f56a512203ad8434727753b80b55d0b807ca46fb
Brase <brasea@wwt.com>
2022-07-15 08:55:44 -0500

    learning about control flow

---
+++ 04 javascript_basics/05 Control Flow.js
@@ -0,0 +1,13 @@
+var hot = false;
+var temp = 20;
+
+if (temp>80) {
+    alert("it is hot")
+    console.log("temp is greater than 80")
+}else if (temp<80){
+    alert("it is not hot")
+    console.log("temp is less than 80")
+}else{
+    alert("no info")
+    console.log("no info")
+}
