7e5cf3a21b403d3bf325335efd53b5ee9ee516d5
Brase <brasea@wwt.com>
2022-07-15 10:19:03 -0500

    learning about control flow

--- 04 javascript_basics/05 Control Flow.js
+++ 04 javascript_basics/05 Control Flow.js
@@ -1,16 +1,19 @@
 var hot = false;
 var temp = prompt("what temp is it outside?")
 
-if (temp>80) {
+if (temp>=80) {
     alert("it is hot")
     hot = true
-    console.log("temp is greater than 80")
-}else if (temp<80){
-    alert("it is not hot")
-    console.log("temp is less than 80")
+    console.log("temp is hot")
+}else if (temp<80 && temp>=50){
+    alert("it is normal")
+    console.log("temp is normal")
+}else if (temp<80 && temp>32){
+    alert("it is cold")
+    console.log("temp is cold")
 }else{
-    alert("no info")
-    console.log("no info")
+    alert("it is freezing")
+    console.log("it is freezing")
 }
 
 if (hot == true){
