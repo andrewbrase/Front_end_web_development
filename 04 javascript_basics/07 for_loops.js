d0c6d95bea06e89235a9b4b10aa1c1eddad5425f
Brase <brasea@wwt.com>
2022-07-15 12:19:17 -0500

    started learning for loops in JS

--- 04 javascript_basics/07 for_loops.js
+++ 04 javascript_basics/07 for_loops.js
@@ -3,3 +3,13 @@ alert("for loops")
 for (var i = 0; i < 5; i++){
     console.log("number is " + i);
 }
+
+// sets var to string "Andrew", if the index 
+// of word is less that the word length, 
+// index + 1 --> alert indexed value
+
+var word = "Andrew"
+
+for (var i = 0; i < word.length; i++){
+    console.log(word[i]);
+}
