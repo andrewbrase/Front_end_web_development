2d654d17978a261ea329cdf2fa546f8e43e921d4
Brase <brasea@wwt.com>
2022-07-15 10:58:25 -0500

    learning about control flow

---
+++ 04 javascript_basics/06 while_loops.js
@@ -0,0 +1,17 @@
+alert("while loops")
+
+var x = 0;
+
+while (x < 5){
+    console.log("x is currently " + x);
+
+    if (x===3){
+        console.log("x is equal to three");
+        break
+        // this will break out of the top level loop that it is in
+    }
+    console.log("x is still less than 5, adding 1 to x");
+    x = x+1;
+}
+
+console.log("x is now " + x)
