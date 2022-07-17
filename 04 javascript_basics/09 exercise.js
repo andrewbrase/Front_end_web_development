4516040bb1f0deb87e0f086357b7e8a567d75058
Brase <brasea@wwt.com>
2022-07-16 21:58:49 -0500

    finished exercise 09 spy problem

--- 04 javascript_basics/09 exercise.js
+++ 04 javascript_basics/09 exercise.js
@@ -2,8 +2,8 @@ var firstName= prompt("what is your first name").toUpperCase()
 var lastName= prompt("what is your last name").toUpperCase()
 var age= prompt("what is your age")
 var tall= prompt ("how tall are you in cm?")
-var penName= prompt("what is the name of your pet").toUpperCase()
-var lenval = penName.length
+var petName= prompt("what is the name of your pet").toUpperCase()
+var lenval = petName.length
 
 // JavaScript's toUpperCase() method converts a string 
 // object into a new string consisting of the contents 
@@ -12,7 +12,7 @@ var lenval = penName.length
 if (firstName[0] === lastName[0]){
     if (age > 20 && age < 30){
         if (tall > 169){
-            if (penName[lenval - 1] === "Y"){
+            if (petName[lenval - 1] === "Y"){
                 console.log("secret message");
             }
         }
