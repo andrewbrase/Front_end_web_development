656bf3772eaa214b3f1191aa1b8b9a87ac19dc12
Brase <brasea@wwt.com>
2022-07-17 19:44:08 -0500

    started working on learning on how to write functions in JS

---
+++ 04 javascript_basics/10 functions.js
@@ -0,0 +1,13 @@
+function helloName(name){
+    alert("hello " + name);
+}
+
+helloName(prompt("what is your name?"))
+
+first = parseInt(prompt("pick a number"))
+second = parseInt(prompt("pick another number"))
+
+addNum(first,second)
+function addNum(num1,num2){
+    alert(num1+num2);
+}
