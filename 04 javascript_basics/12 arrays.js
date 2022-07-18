491bea24a0d16ecba016526c041ce436094c2420
Brase <brasea@wwt.com>
2022-07-18 12:49:26 -0500

    started on arrays in JS

---
+++ 04 javascript_basics/12 arrays.js
@@ -0,0 +1,63 @@
+// // var country1 = "USA";
+// // var country2 = "Germany";
+// // var country3 = "China";
+
+// var countries = ["USA",
+//                 "Germany",
+//                 "China"]
+
+// // accessing elements in an array - the elements are in
+// // a sequence that can be indexed
+
+// alert(countries[0]);
+
+// countries[2] = "France";
+
+// alert(countries[2]);
+
+// var word = "andrew"
+
+// word[0] = "A"
+
+// alert(word)
+// WORD WILL STILL BE andrew without uppercase A - 
+// because a string is immutable in this situation
+
+// the array is mutable - string is immutable
+
+// mixed data types - arrays can have booleans, ints, 
+// strings etc differtent data types
+
+// var mixed = [1,"a",false];
+// alert(mixed.length) ---> 3
+
+// array methods in JS
+
+var myArr = ['one','two',"three"];
+
+// to add and remove elements in an array
+// use the push and pop methods
+
+// var lastItem = myArr.pop()
+// alert(lastItem) ---> "three"
+
+myArr.push("four");
+// alert(myArr);
+// ---> "one" "two" "three" "four"
+
+// how to index last item in array
+var lastItemIn= myArr[myArr.length - 1];
+alert(lastItemIn)
+// ---> "four"
+
+// array iteration
+// could use a for loop
+// for(var i = 0; i < myArr.length; i++){
+//     console.log(myArr[i]);
+// }
+
+// also the for of statement
+// For Of
+for(letter of myArr){
+    console.log(letter);
+}
