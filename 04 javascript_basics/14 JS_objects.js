47fb7592a8f88582be2409c0655a63e16aea41d7
Brase <brasea@wwt.com>
2022-07-19 14:17:46 -0500

    started learning about JS Objects

--- 04 javascript_basics/14 JS_objects.js
+++ 04 javascript_basics/14 JS_objects.js
@@ -31,13 +31,100 @@ var newJsObj = {a:"hello",b:[1,2,3],c:{inside:['a','b']}}
 // to iterate through an object
 // use a for in
 
-// for (key in carInfo){
-//     // there is no order in objects --> sometimes the order might be different
-//     alert(key)
-for (k in carInfo){
-// grabbing keys
-console.log(k)
-// grabbing values
-console.log(carInfo[k])
-}
+// // for (key in carInfo){
+// //     // there is no order in objects --> sometimes the order might be different
+// //     alert(key)
+// for (k in carInfo){
+// // grabbing keys
+// console.log(k)
+// // grabbing values
+// console.log(carInfo[k])
 // }
+// // }
+
+// Object methods - functions that are inside of an object
+// var carInfo = {
+//     make:"Toyota",
+//     year:1990, 
+//     model:"Camry", 
+//     carAlert: function(){
+//     alert("BEEP");
+//     // key is car alert
+//     // the value is a function call
+//    }
+// };
+
+// you can call methods off of objects, they act as functions inside of an object
+// use the this keyword
+// For a JS Object, the this is set to the object the method is called on
+
+// var myObj = {
+//     prop:37,
+//     reportProp: function() {
+//         return this.prop;
+//     }
+// };
+// console.log(myObj.reportProp()); // logs 37
+
+// !!!
+// var newJs = {
+//     my_name : "Andrew",
+//     conlog : function() {
+//         return this.my_name;
+//     }
+// };
+// alert(newJs.conlog()) --> Andrew
+// console.log(newJs.conlog()); --> Andrew
+
+// !!!
+// var simple = {
+//     prop : "Hello",
+//     myMethod : function(){
+//         alert("The myMethod was called")
+//     }
+// }
+
+// console.dir(simple)
+// simple.myMethod() --> "The myMethod was called"
+
+// to show to use of the this keyword
+
+var myObj_ = {
+    name : "Jose",
+    greet : function(){
+        // this acts as a PY->self but for JS?
+        // a method that uses properties of the object that it's in
+        console.log("hello " + this.name)
+    }
+}
+
+myObj_.greet()
+
+// ???
+// let allows you to declare variables that are limited to the scope of a block statement,
+//  or expression on which it is used, unlike the var keyword, which declares a variable 
+//  globally, or locally to an entire function regardless of block scope.
+
+// OOP in JS
+
+// let names = {
+//     fname: "Dillion",
+//     lname: "Megida"
+// }
+// console.log(names.fname);
+// console.log(names.hasOwnProperty("mname"));
+// // Expected Output
+// // Dillion
+// // false
+
+// VS OOP in Python
+
+// class Parrot:
+
+//     # class attribute
+//     species = "bird"
+
+//     # instance attribute
+//     def __init__(self, name, age):
+//         self.name = name
+//         self.age = age
