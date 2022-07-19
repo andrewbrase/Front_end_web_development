311d2c77a7349e154a98a36e9ce6c91915d974ef
Brase <brasea@wwt.com>
2022-07-19 08:03:19 -0500

    started working on roster problem exercise

--- 04 javascript_basics/13 Array_Exercise.js
+++ 04 javascript_basics/13 Array_Exercise.js
@@ -2,8 +2,6 @@
 // This is  a .js file with commented hints, its optional to use this.
 
 // Create Empty Student Roster Array
-// This has been done for you!
-var roster = []
 
 // Create the functions for the tasks
 
@@ -11,7 +9,6 @@ var roster = []
 
 // Create a function called addNew that takes in a name
 // and uses .push to add a new student to the array
-
 
 // REMOVE STUDENT
 
@@ -23,10 +20,46 @@ var roster = []
 
 // DISPLAY ROSTER
 
-// Create a function called display that prints out the orster to the console.
-
+// Create a function called display that prints out the roster to the console.
 
 // Start by asking if they want to use the web app
 
 // Now create a while loop that keeps asking for an action (add,remove, display or quit)
 // Use if and else if statements to execute the correct function for each command.
+
+var choice = prompt("Do you want to use the web app? Y or N")
+if (choice.toUpperCase() === "Y"){
+    
+    var roster = []
+
+    function addNew(name){
+        roster.push(name)
+    }
+
+    function removeName(name){
+        roster.splice(roster.indexOf(name),1);
+    }
+
+    function display(){
+        console.log(roster)
+    }
+
+    var on = true;
+    while (on){
+        var action = prompt( "please select an action (add,remove, display or quit)")
+        if (action.toUpperCase() === "ADD"){
+            var newname = prompt("please provide a name to add")
+            addNew(newname)
+        }
+        if (action.toUpperCase() === "REMOVE"){
+            var ridname = prompt("please provide a name to remove")
+            removeName(ridname)
+        }
+        if (action.toUpperCase() === "DISPLAY"){
+            display()
+        }
+        if (action.toUpperCase() === "QUIT"){
+            on = false
+        }
+    }
+}
