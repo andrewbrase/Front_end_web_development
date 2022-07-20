9903af94c62361cc43d79952f04cb7fee4981b6f
Brase <brasea@wwt.com>
2022-07-20 18:24:13 -0500

    finished random color picker exercise using DOM

--- 04 javascript_basics/15 Document_Object_model.js
+++ 04 javascript_basics/15 Document_Object_model.js
@@ -1,4 +1,21 @@
 alert("DOM lesson")
 
-// var myHeader = document.querySelector("h1");
+var myHeader = document.querySelector("h1");
 // myHeader.style.color = "red";
+
+function getRandomColor(){
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+
+    }
+    return color
+}
+
+function changeHeaderColor(){
+    colorInput = getRandomColor()
+    myHeader.style.color = colorInput;
+}
+
+setInterval("changeHeaderColor()",500);
