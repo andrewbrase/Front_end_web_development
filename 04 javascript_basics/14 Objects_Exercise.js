c78db760da2d883b11b4bd9701c60bc8b78eb052
Brase <brasea@wwt.com>
2022-07-20 13:06:40 -0500

    completed objects exercise

--- 04 javascript_basics/14 Objects_Exercise.js
+++ 04 javascript_basics/14 Objects_Exercise.js
@@ -8,8 +8,15 @@
 var employee = {
   name: "John Smith",
   job: "Programmer",
-  age: 31
+  age: 31,
+
+  nameLength : function(){
+    console.log(this.name.length)
+  }
 }
+
+employee.nameLength()
+// --> 10 
 
 // Add a method called nameLength that prints out the
 // length of the employees name to the console.
@@ -23,8 +30,13 @@ var employee = {
 var employee = {
   name: "John Smith",
   job: "Programmer",
-  age: 31
+  age: 31,
+  Alert : function(){
+    alert("Name is " + this.name + ", Job is " + this.job + ", Age is " + this.age)
+  }
 }
+
+employee.Alert()
 
 // Write program that will create an Alert in the browser of each of the
 // object's values for the key value pairs. For example, it should alert:
@@ -41,8 +53,15 @@ var employee = {
 var employee = {
   name: "John Smith",
   job: "Programmer",
-  age: 31
+  age: 31,
+  lastName : function(){
+    nameList = this.name.split(" ")
+    console.log(nameList[1])
+  }
 }
+
+employee.lastName()
+// --> Smith
 
 // Add a method called lastName that prints
 // out the employee's last name to the console.
