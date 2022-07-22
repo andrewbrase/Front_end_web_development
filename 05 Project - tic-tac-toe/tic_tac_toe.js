bd1297fed9dc1cb1937a1d973238878d07666cbd
Brase <brasea@wwt.com>
2022-07-21 20:30:17 -0500

    just finished adding the change background feature to the board

---
+++ 05 Project - tic-tac-toe/tic_tac_toe.js
@@ -0,0 +1,88 @@
+var boxone = document.querySelector("#b1");
+var boxtwo = document.querySelector("#b2");
+var boxthree = document.querySelector("#b3");
+var boxfour = document.querySelector("#b4");
+var boxfive = document.querySelector("#b5");
+var boxsix = document.querySelector("#b6");
+var boxseven = document.querySelector("#b7");
+var boxeight = document.querySelector("#b8");
+var boxnine = document.querySelector("#b9");
+
+// SQUARE ONE
+boxone.addEventListener("mouseover",function(){
+    boxone.style.backgroundColor = "green";
+})
+boxone.addEventListener("mouseout",function(){
+    boxone.style.backgroundColor = "#d4d4d4";
+})
+//
+// SQUARE TWO
+boxtwo.addEventListener("mouseover",function(){
+    boxtwo.style.backgroundColor = "green";
+})
+boxtwo.addEventListener("mouseout",function(){
+    boxtwo.style.backgroundColor = "#d4d4d4";
+})
+
+//
+// SQUARE THREE
+boxthree.addEventListener("mouseover",function(){
+    boxthree.style.backgroundColor = "green";
+})
+boxthree.addEventListener("mouseout",function(){
+    boxthree.style.backgroundColor = "#d4d4d4";
+})
+
+//
+// SQUARE FOUR
+boxfour.addEventListener("mouseover",function(){
+    boxfour.style.backgroundColor = "green";
+})
+boxfour.addEventListener("mouseout",function(){
+    boxfour.style.backgroundColor = "#d4d4d4";
+})
+
+// 
+// SQUARE FIVE
+boxfive.addEventListener("mouseover",function(){
+    boxfive.style.backgroundColor = "green";
+})
+boxfive.addEventListener("mouseout",function(){
+    boxfive.style.backgroundColor = "#d4d4d4";
+})
+
+// 
+// SQUARE SIX
+boxsix.addEventListener("mouseover",function(){
+    boxsix.style.backgroundColor = "green";
+})
+boxsix.addEventListener("mouseout",function(){
+    boxsix.style.backgroundColor = "#d4d4d4";
+})
+
+// 
+// SQUARE SEVEN
+boxseven.addEventListener("mouseover",function(){
+    boxseven.style.backgroundColor = "green";
+})
+boxseven.addEventListener("mouseout",function(){
+    boxseven.style.backgroundColor = "#d4d4d4";
+})
+
+// 
+// SQUARE EIGHT
+boxeight.addEventListener("mouseover",function(){
+    boxeight.style.backgroundColor = "green";
+})
+boxeight.addEventListener("mouseout",function(){
+    boxeight.style.backgroundColor = "#d4d4d4";
+})
+
+// 
+// SQUARE NINE
+boxnine.addEventListener("mouseover",function(){
+    boxnine.style.backgroundColor = "green";
+})
+boxnine.addEventListener("mouseout",function(){
+    boxnine.style.backgroundColor = "#d4d4d4";
+})
